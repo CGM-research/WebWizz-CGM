50a7b264e3a78c14c3dc36748278fe1eb75ccdf9
John Afanasieff <118971918+Wafflelover404@users.noreply.github.com>
2024-10-24 10:40:12 +0000

    Added banner

--- welcome_page.py
+++ welcome_page.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.title("Welcome to *WebWizz* ! :wave:")
 st.subheader("A simple website editor with cool functions of generating web-pages powered by AI algorithms")
 st.markdown("This project is built with <a href='https://streamlit.io/'>Streamlit :crown:</a> and <a href='https://huggingface.co/'>HuggingFace :hugging_face:</a>.", unsafe_allow_html=True)
-
-st.link_button("Contact us !", "https://google.com")
+st.image(image="ExtraFiles/banner.png")
+st.link_button("Join us !", "https://t.me/webwizzcom")
