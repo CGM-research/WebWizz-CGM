716ed5e704cf3312edf9882c22171366ae72f69b
John Afanasieff <118971918+Wafflelover404@users.noreply.github.com>
2024-10-24 14:08:05 +0300

    Update welcome_page.py

--- welcome_page.py
+++ welcome_page.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("Welcome to *WebWizz* ! :wave:")
 st.subheader("A simple website editor with cool functions of generating web-pages powered by AI algorithms")
 st.markdown("This project is built with <a href='https://streamlit.io/'>Streamlit :crown:</a> and <a href='https://huggingface.co/'>HuggingFace :hugging_face:</a>.", unsafe_allow_html=True)
-st.image(image="ExtraFiles/banner.png")
+st.image(image="ExtraFiles/jointg.png")
 st.markdown("You are welcome in our <a href='https://t.me/webwizzcom'>Telegram channel</a> !", unsafe_allow_html=True)
 
