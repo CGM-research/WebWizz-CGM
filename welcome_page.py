8a2487d799749d338fbe95c6f67000c6d36a5c14
John Afanasieff <118971918+Wafflelover404@users.noreply.github.com>
2024-11-01 02:54:16 +0300

    Update welcome_page.py

--- welcome_page.py
+++ welcome_page.py
@@ -5,6 +5,8 @@ st.subheader("A simple website editor with cool functions of generating web-page
 st.markdown("This project is built with <a href='https://streamlit.io/'>Streamlit :crown:</a> and <a href='https://huggingface.co/'>HuggingFace :hugging_face:</a>.", unsafe_allow_html=True)
 st.markdown("You are welcome in our <a href='https://t.me/webwizzcom'>Telegram channel</a> !", unsafe_allow_html=True)
 st.divider()
+st.markdown("Also checkout <a href='https://t.me/ezcodebot'>EzCode bot</a> and <a href='https://t.me/learnezcode'>EzCode telegram channel</a>!", unsafe_allow_html=True)
+st.divider()
 with st.container(border=False):
     st.image(image="ExtraFiles/jointg.png", width=640)
 
