787613c8268f1cc569830eac4b6c008964daa455
John Afanasieff <afanasieffivan@gmail.com>
2024-11-09 10:42:40 +0300

    W commit
    Changed EzCode to ezcode
    Massive perfomance boost

--- welcome_page.py
+++ welcome_page.py
@@ -7,21 +7,21 @@ translations = {
         'subheader': "A simple website editor with cool functions of generating web-pages powered by AI algorithms",
         'markdown1': "This project is built with <a href='https://streamlit.io/'>Streamlit :crown:</a> and <a href='https://huggingface.co/'>HuggingFace :hugging_face:</a>.",
         'markdown2': "You are welcome in our <a href='https://t.me/webwizzcom'>Telegram channel</a> !",
-        'markdown3': "Also checkout <a href='https://t.me/ezcodebot'>EzCode bot</a> and <a href='https://t.me/learnezcode'>EzCode telegram channel</a>!"
+        'markdown3': "Also checkout <a href='https://t.me/ezcodebot'>ezcode bot</a> and <a href='https://t.me/learnezcode'>ezcode telegram channel</a>!"
     },
     'ru': {
         'title': "Добро пожаловать в *WebWizz* ! :wave:",
         'subheader': "Простой редактор веб-сайтов с крутыми функциями генерации веб-страниц на основе алгоритмов искусственного интеллекта",
         'markdown1': "Этот проект создан с использованием <a href='https://streamlit.io/'>Streamlit :crown:</a> и <a href='https://huggingface.co/'>HuggingFace :hugging_face:</a>.",
         'markdown2': "Добро пожаловать в наш <a href='https://t.me/webwizzcom'>Telegram канал</a> !",
-        'markdown3': "Также загляните в <a href='https://t.me/ezcodebot'>EzCode бот</a> и <a href='https://t.me/learnezcode'>Telegram канал EzCode</a>!"
+        'markdown3': "Также загляните в <a href='https://t.me/ezcodebot'>ezcode бот</a> и <a href='https://t.me/learnezcode'>Telegram канал ezcode</a>!"
     },
     'de': {
         'title': "Willkommen bei *WebWizz* ! :wave:",
         'subheader': "Ein einfacher Website-Editor mit coolen Funktionen zur Generierung von Webseiten, unterstützt von KI-Algorithmen",
         'markdown1': "Dieses Projekt wurde mit <a href='https://streamlit.io/'>Streamlit :crown:</a> und <a href='https://huggingface.co/'>HuggingFace :hugging_face:</a> erstellt.",
         'markdown2': "Willkommen in unserem <a href='https://t.me/webwizzcom'>Telegram-Kanal</a> !",
-        'markdown3': "Schauen Sie sich auch <a href='https://t.me/ezcodebot'>EzCode bot</a> und <a href='https://t.me/learnezcode'>EzCode Telegram-Kanal</a> an!"
+        'markdown3': "Schauen Sie sich auch <a href='https://t.me/ezcodebot'>ezcode bot</a> und <a href='https://t.me/learnezcode'>ezcode Telegram-Kanal</a> an!"
     }
 }
 
